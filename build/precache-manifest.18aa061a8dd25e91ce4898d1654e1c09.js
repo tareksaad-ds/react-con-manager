f2339f4046a8c48bc4930e72b943a70c7c6ae723
Tarek <tarekmarketer30@gmail.com>
2020-07-03 20:26:49 +0200

    include build in the repo

---
+++ build/precache-manifest.18aa061a8dd25e91ce4898d1654e1c09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b450051fc75c7b6cb02390593c71a610",
+    "url": "/react-con-manager/index.html"
+  },
+  {
+    "revision": "ac4efe2d11319595f923",
+    "url": "/react-con-manager/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "191daf2e0d9e6e2194a7",
+    "url": "/react-con-manager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "ac4efe2d11319595f923",
+    "url": "/react-con-manager/static/js/2.b8141206.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-con-manager/static/js/2.b8141206.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "191daf2e0d9e6e2194a7",
+    "url": "/react-con-manager/static/js/main.809f1570.chunk.js"
+  },
+  {
+    "revision": "392e2b0e4ae98caa62f0",
+    "url": "/react-con-manager/static/js/runtime-main.32d2f060.js"
+  }
+]);
